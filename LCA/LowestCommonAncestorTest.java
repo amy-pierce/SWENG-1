909cf72a66c9ef1aba549435a971b1c7d48c2bc0
amy-pierce <34216512+amy-pierce@users.noreply.github.com>
2019-09-23 18:27:12 +0100

    declaring test cases

--- LCA/LowestCommonAncestorTest.java
+++ LCA/LowestCommonAncestorTest.java
@@ -6,52 +6,32 @@ import org.junit.Test;
 public class LowestCommonAncestorTest {
 
 
+	@Test 
+	public void testemptyTree() {
+	}
+
+	@Test 
+	public void testOneNodeTree() {
+	}
 	
 	@Test
 	public void testCreateHead(){
-		LowestCommonAncestor lca = new LowestCommonAncestor();
-		lca.createHead('a',8);
-		assertEquals("Create a head", 'a', lca.returnKey(lca.head));
 	}
 	
 	@Test
 	public void testAddNode(){
-		LowestCommonAncestor lca = new LowestCommonAncestor();
-		lca.createHead('d',0);
-		lca.addNode('a',1);
-		assertEquals("Add node a", 'a', lca.returnKey(lca.head.left));
-
 	}
 	
 	@Test
 	public void testFindNode(){
-		LowestCommonAncestor lca = new LowestCommonAncestor();
-		lca.createHead('d',0);
-		assertEquals("Find head Node", 'd', lca.returnKey(lca.findNode('d')));
-		lca.addNode('a',1);
-		assertEquals("Find left Node", 'a', lca.returnKey(lca.findNode('a')));
 	}
 	
 	@Test
 	public void testDepth(){
-		LowestCommonAncestor lca = new LowestCommonAncestor();
-		lca.createHead('d',0);
-		assertEquals("Depth of head", 0, lca.depth('d'));
-		lca.addNode('a',1);
-		assertEquals("Depth of a", 1, lca.depth('a'));
-	
 	}
 
 	
-	@Test
-	public void testFull(){
-		LowestCommonAncestor lca = new LowestCommonAncestor();
-		lca.createHead('a',0);		
-		assertEquals("Should return head", lca.findNode('f'), lca.lowestCommonAncestor(lca.findNode('f'), lca.findNode('f'), null));
-		lca.addNode('g',1);
-		lca.addNode('l',2);
-		assertEquals("Should return head", lca.findNode('f'), lca.lowestCommonAncestor(lca.findNode('l'), lca.findNode('c'), lca.findNode('k')));
-	}
+	
 	
 
 }
