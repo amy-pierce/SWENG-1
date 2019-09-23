2699214b90ec06eb973dcbe966087f3a07681ef6
amy-pierce <34216512+amy-pierce@users.noreply.github.com>
2019-09-23 02:20:24 +0100

    adding basic LCA program with function headers

---
+++ LCA/LowestCommonAncestor.java
@@ -0,0 +1,28 @@
+
+public class LowestCommonAncestor {
+	
+	public class Node{  
+		private char key;
+		public Node left, right, parent;
+		private int value;
+	}
+	
+	public void createHead(char key,int value){
+	
+	}
+	
+	public void addNode(char key, int value){
+
+	}
+	
+	public Node findNode(char key){
+	
+	}
+	
+	public Node lowestCommonAncestor(Node root, Node p, Node q) {
+	 
+	}
+	public static void main(String[] args) {
+	
+	}
+}
