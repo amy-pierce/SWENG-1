f93231faa6163af6ae704f44a9f9a1ea57d91db5
amy-pierce <34216512+amy-pierce@users.noreply.github.com>
2019-09-25 18:04:21 +0100

    adding a some methods for LCA- not fully working

--- LCA/LowestCommonAncestor.java
+++ LCA/LowestCommonAncestor.java
@@ -2,14 +2,24 @@
 public class LowestCommonAncestor {
 	
 	public class Node{  
-		private char key;
-		public Node left, right, parent;
-		private int value;
+		  int val;
+   		 Node left, right;
+ 
+   		 Node(int value) {
+      			  val = value;
+      			  left = right = null;
+  		  }
 	}
 	
-	public void createHead(char key,int value){
-	
-	}
+	static int Depth (LowestCommonAncestor x) {
+		if(x.root==null) {
+			return 0;
+		}
+		if(x.root.left==null&&x.root.right==null) {
+			return 1;
+		}
+		return 2;
+   	}
 	
 	public void addNode(char key, int value){
 
@@ -20,9 +30,42 @@ public class LowestCommonAncestor {
 	}
 	
 	public Node lowestCommonAncestor(Node root, Node p, Node q) {
+		if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {		
+            		return -1;
+     		}
+		
+      		for (int i = 0; i < path1.size() && i < path2.size(); i++) {
+            		if (!path1.get(i).equals(path2.get(i)))
+                		break;
+        	}
+ 
+        	return path1.get(i-1);
 	 
 	}
-	public static void main(String[] args) {
 	
-	}
+	
+	public static void main(String args[]) {  
+		Node root = new Node(1);  
+		root.left = new Node(2);  
+		root.right = new Node(3);  
+
+		root.left.left = new Node(4);  
+		root.left.right = new Node(5);  
+		root.right.left = new Node(6);  
+		root.right.right = new Node(7);  
+
+		root.left.left.left = new Node(8);  
+		root.left.left.right = new Node(9);  
+		root.left.right.left = new Node(10);  
+		root.left.right.right = new Node(11);  
+		root.right.left.left = new Node(12);  
+		root.right.left.right = new Node(13);  
+		root.right.right.left = new Node(14);  
+		root.right.right.right = new Node(15);  
+
+	 
+  	  } 
+	
+	
+	
 }
