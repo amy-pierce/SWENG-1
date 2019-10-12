0ea4603f1a23f068c44b051764783bd200926204
amy-pierce <34216512+amy-pierce@users.noreply.github.com>
2019-10-12 18:37:08 +0100

    adding DAG test for constructor

--- LCA/DAGTest.java
+++ LCA/DAGTest.java
@@ -1,14 +1,24 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class DAGTest {
 
 
+	@Test
+	void testConstructor() {
+		LCA_DAG DAG = new LCA_DAG(6);
+		assertEquals("Testing Constructor", 6, DAG.V());
+		
+		LCA_DAG DAG2 = new LCA_DAG(9);
+		assertEquals("Testing Constructor", 9, DAG2.V());
+		
+		LCA_DAG DAG3 = new LCA_DAG(15);
+		assertEquals("Testing Constructor", 15, DAG3.V());
 
+	}
 
 	@Test
 	void testEandAddEdge() {
