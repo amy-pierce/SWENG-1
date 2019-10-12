468276c57fe24d4c07d5bffe80fd3be9e1eb3f16
amy-pierce <34216512+amy-pierce@users.noreply.github.com>
2019-10-12 18:32:23 +0100

    adding DAG and DAGTest files

---
+++ LCA/DAGTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DAGTest {
+
+
+
+
+	@Test
+	void testEandAddEdge() {
+	}
+
+	@Test
+	void testAcyclic() {
+
+	}
+
+	@Test
+	void testLCA() {
+	
+	}
+
+}
